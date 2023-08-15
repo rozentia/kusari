5d6affe2b2414e2d1e99c4279f5a14a7efb7aecd
Enrique Velasco <h.superpotter@gmail.com>
2023-08-15 15:33:16 +0200

    feat: added fonts and cleared boilerplate

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['var(--font-nunito-sans)'],
+        display: ['var(--font-nunito)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
