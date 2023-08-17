40f224b7a23a4900d9bb568621d35c8900923430
Enrique Velasco <h.superpotter@gmail.com>
2023-08-17 09:44:45 +0200

    feat: add prismic preview support

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import clsx from 'clsx'
 import { nunito, nunitoSans } from '@/fonts'
-import { createClient } from '@/prismicio'
+import { createClient, repositoryName } from '@/prismicio'
+import { PrismicPreview } from '@prismicio/next'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <PrismicPreview repositoryName={repositoryName} />
         </body>
     </html>
   )
